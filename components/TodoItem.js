e183d028288a9e2aaaa63de1a0e42ccae50f9a14
keith brooks <kbrook10@gmail.com>
2016-10-31 15:05:46 -0400

    Update the base files again

---
+++ components/TodoItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+const TodoItem = (props) =>
+(
+    <div>
+        <div className="checkbox">
+          <label className="input-group" onClick={props.markDone}>
+            <input type="checkbox" checked={props.item.done}/>
+            <span id="newTask" style={{textDecoration:props.item.done?'line-through':''}}>{props.item.text}</span>
+            <span className="input-group-btn">
+              <button id="removeTaskItem" className="btn btn-default text-right">Delete</button>
+            </span>
+          </label>
+        </div>
+    </div>
+)
+export default TodoItem
