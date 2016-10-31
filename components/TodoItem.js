b4b5e27d0bf917eaed5530fb813e282d3ed82de4
keith brooks <kbrook10@gmail.com>
2016-10-31 16:11:03 -0400

    Update Todo Task List

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,17 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const TodoItem = (props) =>
-(
-    <div>
-        <div className="checkbox">
-          <label className="input-group" onClick={props.markDone}>
-            <input type="checkbox" checked={props.item.done}/>
-            <span id="newTask" style={{textDecoration:props.item.done?'line-through':''}}>{props.item.text}</span>
-            <span className="input-group-btn">
-              <button id="removeTaskItem" className="btn btn-default text-right">Delete</button>
-            </span>
-          </label>
-        </div>
-    </div>
-)
+const TodoItem = (props) => {
+    return <li className="list-group-item">{props.description}</li>
+}
+
 export default TodoItem
--- js/global.js
+++ js/global.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Todos from '../components/Todos'
+import React from 'react'; //import the react node library
+import ReactDOM from 'react-dom'; //import the react-dom node library
+import Todos from '../components/Todos'; // request App react component
 
-//Task(): Create renderView function to refresh the M 'V' C and change the HTML tags on page
+//Task(): Create event listener for the 'keypress' of enter and the 'add' button
 //<---------------------------------------------------------------------------->
-function renderView(){
-    ReactDOM.render( //'ReactDOM' is just a placeholder and render is the method to run...
-        <Todos/>, document.getElementById('todos') //This takes (2) arguments a component and an ID
+//Create global array...
+var todos = []
+
+//Create variable for 'Add Button' and 'Keypress'
+var keyPress = document.getElementById('addInputField')
+var clickBtn = document.getElementById('addBtn')
+
+//Event Listener for the keyPress on the 'input field' and Add Button on 'click'
+keyPress.addEventListener('keypress', pressEnter);
+clickBtn.addEventListener('click', addTodo);
+
+//Create function so that the 'addTodo' only runs once the enter key is pressed and not after every keystroke...
+function pressEnter(event) {
+    if (event.key === 'Enter'){
+        alert('we got here')
+        addTodo();
+    };
+}
+
+//<---------------------------------------------------------------------------->
+//<--------------------------End of Task--------------------------------------->
+
+//Task(): Create function to add input field content to the global array 'todos'
+//<---------------------------------------------------------------------------->
+function addTodo(){
+    var addInputValue = keyPress.value
+    keyPress.value = ''
+    todos.push({
+        item: addInputValue,
+    })
+    console.log(addInputValue)
+    renderView(todos)
+}
+
+//<---------------------------------------------------------------------------->
+//<--------------------------End of Task--------------------------------------->
+
+//Task(): Render View
+//<---------------------------------------------------------------------------->
+function renderView(arrayTodoItems){
+    ReactDOM.render(
+        <Todos taskList={arrayTodoItems} />, document.getElementById('todoItems')
     )
 }
-renderView()
-
-
+// renderView()
 //<---------------------------------------------------------------------------->
 //<--------------------------End of Task--------------------------------------->
 
-//Task():
-//<---------------------------------------------------------------------------->
 
-//<---------------------------------------------------------------------------->
-//<--------------------------End of Task--------------------------------------->
-
-//Task():
-//<---------------------------------------------------------------------------->
-
-//<---------------------------------------------------------------------------->
-//<--------------------------End of Task--------------------------------------->
-
-//Task():
-//<---------------------------------------------------------------------------->
 
 //<---------------------------------------------------------------------------->
 //<--------------------------End of Task--------------------------------------->
