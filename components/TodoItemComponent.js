35e6588234170e1d0951c7da33ea56bf82b0d075
keith brooks <kbrook10@gmail.com>
2016-10-31 14:59:25 -0400

    Update from existing point to resolve incomplete assignment

--- components/TodoItemComponent.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-
-const TodoItemComponent = (props) => {
-    return <li className="list-group-item">{props.description}</li>
-}
-
-export default TodoItemComponent
